2a583a8ec95fc7cedb9bedf5b30f08c37a33f59b
Greg Towne <townegr@yahoo.com>
2014-04-19 18:36:36 -0400

    add score

--- ball.rb
+++ ball.rb
@@ -29,11 +29,33 @@ class Ball
     @ball.move!
   end
 
-  def move!
-    dx = Gosu.offset_x(angle, speed)
-    dy = Gosu.offset_y(angle, speed)
+  def dx; Gosu.offset_x(angle, speed); end
+  def dy; Gosu.offset_y(angle, speed); end
 
+  def move!
     @x += dx
     @y += dy
+
+    if @y < 0
+      @y = 0
+      bounce_off_edge!
+    end
+
+    if @y > Pong::HEIGHT
+      @y = Pong::HEIGHT
+      bounce_off_edge!
+    end
+  end
+
+  def off_left?
+    x1 < 0
+  end
+
+  def off_right?
+    x2 > Pong::WIDTH
+  end
+
+  def bounce_off_edge!
+    @angle = Gosu.angle(0, 0, dx, -dy)
   end
 end
